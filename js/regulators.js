3f5ea69bd78121625b191eb1cae276a8c3745797
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-11-28 21:22:31 +0000

    Add regulators

---
+++ js/regulators.js
@@ -0,0 +1,8 @@
+class Regulator {
+    constructor() {
+        this.activatedBy = [];
+        this.activates = [];
+        this.previousActivation = 0;
+        this.currentActivation = 0;
+    }
+}
--- js/shexCells.js
+++ js/shexCells.js
@@ -2,6 +2,12 @@ class Cell {
     constructor() {
         this.energy = 100;
         this.protein = 100;
+        this.regulators = [
+            new Regulator(),
+            new Regulator(),
+            new Regulator(),
+            new Regulator(),
+        ];
     }
 
     update() {
