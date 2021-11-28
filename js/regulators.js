72fa6acea2268721cd366573da86753f5c4f298d
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-11-28 22:12:38 +0000

    Create regulator

--- js/regulators.js
+++ js/regulators.js
@@ -1,8 +1,25 @@
 class Regulator {
-    constructor() {
-        this.activatedBy = [];
-        this.activates = [];
+    constructor(cell) {
+        this.cell = cell;
+        this.regulatedBy = [];
+        this.regulates = [];
         this.previousActivation = 0;
         this.currentActivation = 0;
+    }
+
+    // Add an input causing this regulator to be actived/inhibited by another body
+    addInput(activator, weight, baseline) {
+        this.regulatedBy.push([activator, weight, baseline]);
+    }
+
+    // Add an output that this regulator regulate
+    addOutput(activator, weight, baseline) {
+        this.regulates.push([activator, weight, baseline]);
+    }
+
+    update() {
+        this.currentActivation = 0;
+        this.regulatedBy.forEach(obj => )
+        this.previousActivation = this.currentActivation;
     }
 }
